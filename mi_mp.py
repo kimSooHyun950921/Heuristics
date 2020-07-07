8173945e957b6c452efd0a7e0e16db18af627331
kimSooHyun950921 <soohyunkim950921@gmail.com>
2020-07-07 16:21:20 +0900

    fix bug: apply multiprocessing

--- mi_mp.py
+++ mi_mp.py
@@ -17,35 +17,23 @@ def get_rpc():
     return AuthServiceProxy(f'http://{rpc_user}:{rpc_password}@{rpc_ip}:{rpc_port}', timeout=timeout)
 
 
+def find_non_cluster_addr(addrs):
+    non_cluster_addr_list = []
+    for addr in addrs:
+        cluster_num = cdq.get_cluster_number([addr]).pop()
+        if cluster_num == -1:
+            non_cluster_addr_list.append(cluster_num)
+    return non_cluster_addr_list
+
+
 def is_mi_cond(in_addrs, out_addrs):
     if in_addrs == None or out_addrs == None:
         return False
-    if len(out_addrs) <=0:
-        return False
-    if len(in_addrs) < 2:
-        return False
-    if len(out_addrs) > 2:
-        return False
-    return True
-    
-    
-def get_min_cluster_num(addr, flag=0):
-    ''' DON'T USE
-        flag: 0 전체 최소값
-        flag: 1 -1이 아닌 최소값'''
-    cluster_num_list = cdq.get_min_cluster(addr)
-    cluster_num_list = list()
-    for addr in addr_set.keys():
-        cluster_num_list.append(addr_set[addr])
-    sort_cls_num_list = sorted(cluster_num_list)
-    if flag == 0:
-        return sort_cls_num_list[0]
-    elif flag == 1:
-        for num in sort_cls_num_list:
-            if num > -1:
-                return num
-            
-    
+    if len(in_addrs) >= 2 and len(out_addrs) == 1:
+        return True
+    return False
+
+                
 def get_cluster_num(addrs, max_cluster_num):
     cls_num = -1
     cls_num_set = set(cdq.get_cluster_number(addrs))
@@ -72,21 +60,10 @@ def update_cluster(addrs, cluster_num):
         print(e)
         return False
     
-    
-def is_mi_cond(in_addrs, out_addrs):
-    if in_addrs == None or out_addrs == None:
-        return False
-    if len(out_addrs) <= 0:
-        return False
-    if len(in_addrs) < 2:
-        return False
-    if len(out_addrs) > 2:
-        return False
-    return True
-
-
+        
 def add_db(c_dict):
     '''
+     database에 update 해야함
      1. db에서 주소에 해당하는 값이 모두 동일하다면
          - 클러스터 번호설정 ==> 그대로 냅두면 되는듯
          - 클러스터 번호가 -1 이라면 max값 설정
@@ -94,12 +71,18 @@ def add_db(c_dict):
          - -1이 아닌 최소값으로 설정
      3. 클러스터번호에 설정하는 주소가 있다
     '''
+    if len(c_dict) == 0:
+        print("NO INPUTS!")
+        return
+    max_cluster_num = cdq.get_max_clustered()
     for _, addrs in c_dict.items():
         cluster_num_list = sorted(list(cdq.get_cluster_number(addrs)))
-        if len(cluster_num_list) == 1 and cluster_num_list[0] == -1:
-            cluster_num = cdq.get_max_clustered() + 1
-            execute_list = list(zip([cluster_num]*len(addrs), addrs))
-            cdq.update_cluster_many(execute_list)
+        if len(cluster_num_list) == 1:
+            if cluster_num_list[0] == -1:
+                cluster_num = max_cluster_num + 1
+                max_cluster_num = cluster_num
+                execute_list = list(zip([cluster_num]*len(addrs), addrs))
+                cdq.update_cluster_many(execute_list)
         else:
             cluster_num = -1
             for num in cluster_num_list:
@@ -108,13 +91,17 @@ def add_db(c_dict):
                     break
             for num in cluster_num_list:
                 if num != cluster_num:
-                    addr = cdq.find_addr_from_cluster_num(num)
+                    if num != -1:
+                        addr = cdq.find_addr_from_cluster_num(num)
+                    else:
+                        addr = find_non_cluster_addr(addrs)
                 else:
                     addr = addrs
+                    
                 execute_list = list(zip([cluster_num]*len(addr), addr))
                 cdq.update_cluster_many(execute_list)
-            
-             
+
+                
 def rpc_command(height):
     while True:
         try:
@@ -136,7 +123,6 @@ def multi_input(height):
         tx_indexes = dq.get_txid(tx)
         in_addrs = dq.get_addr_txin(tx_indexes)
         out_addrs = dq.get_addr_txout(tx_indexes)
-        
         if is_mi_cond(in_addrs, out_addrs):
             ##### update cluster dict #################
             '''
@@ -147,7 +133,7 @@ def multi_input(height):
             '''
             need_new_cls_num = True
             for key, addr_set in cluster_dict.items():
-                if len(addr_set & in_addrs) != 0:
+                if len(addr_set & in_addrs) > 0:
                     cluster_dict[key].union(in_addrs)
                     need_new_cls_num = False
                     break
@@ -158,18 +144,18 @@ def multi_input(height):
                     cls_num_set = sorted(list(cluster_dict.keys()))
                     cls_num = set(cls_num_set).pop() + 1
                 cluster_dict.update({cls_num:in_addrs})
-            ############################################ 
+            ############################################    
     return cluster_dict
     
     
 def main():
     term = 10000
     start_height = 0
-    end_height = dq.get_max() - 1
+    end_height = dq.get_max_height()
     pool_num = multiprocessing.cpu_count()//2  
-    
     print("CLSUTER TABLE MADE")
     time.sleep(5)
+    
     stime = time.time()
     try:
         for sheight, eheight in zip(range(start_height, end_height, term), \
@@ -190,6 +176,8 @@ def main():
                             if len(addr_dict[j] & cluster_dict[i]) > 0:
                                 addr_dict[j] = addr_dict[j].union(cluster_dict[i])
                                 cluster_set = cluster_set - {i}
+                if len(list(addr_dict.keys())) > 1:
+                    max_cluster_num = sorted(list(addr_dict.keys())).pop()+1
                 for i in list(cluster_set):
                     addr_dict[max_cluster_num] = \
                     addr_dict.get(max_cluster_num, set()).union(cluster_dict[i])
